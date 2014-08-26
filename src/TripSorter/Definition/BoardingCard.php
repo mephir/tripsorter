94f3788212545a8375537eb51372ef70d3d131ce
Pawel Wilk <pwilkmielno@gmail.com>
2014-08-26 11:17:00 +0200

    boarding card definition

---
+++ src/TripSorter/Definition/BoardingCard.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace TripSorter\Definition;
+
+/**
+ * Abstract class Boarding Card
+ *
+ * @author Pawel Wilk <pwilkmielno@gmail.com>
+ *
+ */
+abstract class BoardingCard
+{
+    protected $departure;
+    protected $arrival;
+
+    /**
+     * Return string representation of Boarding Card
+     *
+     * @param BoardingCard $lastCard
+     *
+     * @return string
+     */
+    abstract public function getDescription(BoardingCard $lastCard = null);
+
+    /**
+     * Constructor
+     *
+     * @param string $departure
+     * @param string $arrival
+     */
+    public function __construct($departure, $arrival)
+    {
+        $this->departure = $departure;
+        $this->arrival = $arrival;
+    }
+
+    /**
+     * Method return departure station
+     *
+     * @return string
+     */
+    public function getDeparture()
+    {
+        return $this->departure;
+    }
+
+    /**
+     * Method set departure station
+     *
+     * @param string $departure
+     *
+     * @return \TripSorter\Definition\BoardingCard
+     */
+    public function setDeparture($departure)
+    {
+        $this->departure = $departure;
+
+        return $this;
+    }
+
+    /**
+     * Method returns arrival station
+     *
+     * @return string
+     */
+    public function getArrival()
+    {
+        return $this->arrival;
+    }
+
+    /**
+     * Method sets arrival station
+     *
+     * @param string $arrival
+     *
+     * @return \TripSorter\Definition\BoardingCard
+     */
+    public function setArrival($arrival)
+    {
+        $this->arrival = $arrival;
+
+        return $this;
+    }
+
+    /**
+     * Returns if luggage should be transfered to another travel point if possible
+     *
+     * @return boolean
+     */
+    public function isBaggageTransfered(BoardingCard $card)
+    {
+        return false;
+    }
+}
